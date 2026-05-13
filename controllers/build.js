95599979249513a475c4ca73fb04a4e8daaf9872
BhowanKhawas <khawasb@roehampton.ac.uk>
2026-05-13 19:52:33 +0100

    Fixed build error and deployed presentation mode

--- controllers/build.js
+++ controllers/build.js
@@ -8,14 +8,18 @@ const artData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
 // 2. Build the Gallery Page (index.html)
 const indexViewPath = path.join(__dirname, '../views/index.pug');
-const compileIndex = pug.compileFile(indexViewPath);
-const indexHtml = compileIndex({ artworks: artData });
+const indexHtml = pug.renderFile(indexViewPath, { artworks: artData });
 fs.writeFileSync(path.join(__dirname, '../index.html'), indexHtml);
 
 // 3. Build the About Page (about.html)
 const aboutViewPath = path.join(__dirname, '../views/about.pug');
-const compileAbout = pug.compileFile(aboutViewPath);
-const aboutHtml = compileAbout(); 
+const aboutHtml = pug.renderFile(aboutViewPath); 
 fs.writeFileSync(path.join(__dirname, '../about.html'), aboutHtml);
 
-console.log('Success! Both index.html and about.html have been generated.');
+// 4. Build the Presentation Page (presentation.html)
+// Make sure you have created views/presentation.pug first!
+const presViewPath = path.join(__dirname, '../views/presentation.pug');
+const presHtml = pug.renderFile(presViewPath, { artworks: artData });
+fs.writeFileSync(path.join(__dirname, '../presentation.html'), presHtml);
+
+console.log('Success! Index, About, and Presentation pages generated.');
