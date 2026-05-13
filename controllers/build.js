3f1f232301f2e6df6f6406a07df0968a43a2ce14
BhowanKhawas <khawasb@roehampton.ac.uk>
2026-05-13 18:44:10 +0100

    Full gallery design upgrade with dark theme and masonry grid

--- controllers/build.js
+++ controllers/build.js
@@ -2,19 +2,20 @@ const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
-// 1. Interacting with the Model (Read the JSON data)
+// 1. Get the Art Data
 const dataPath = path.join(__dirname, '../models/artData.json');
 const artData = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
-// 2. Setting up the View (Compile the Pug file)
-const viewPath = path.join(__dirname, '../views/index.pug');
-const compiledFunction = pug.compileFile(viewPath);
+// 2. Build the Gallery Page (index.html)
+const indexViewPath = path.join(__dirname, '../views/index.pug');
+const compileIndex = pug.compileFile(indexViewPath);
+const indexHtml = compileIndex({ artworks: artData });
+fs.writeFileSync(path.join(__dirname, '../index.html'), indexHtml);
 
-// 3. The Logic (Render the HTML with the data)
-const finalHtml = compiledFunction({ artworks: artData });
+// 3. Build the About Page (about.html)
+const aboutViewPath = path.join(__dirname, '../views/about.pug');
+const compileAbout = pug.compileFile(aboutViewPath);
+const aboutHtml = compileAbout(); 
+fs.writeFileSync(path.join(__dirname, '../about.html'), aboutHtml);
 
-// 4. Output (Save the generated HTML to the root folder for GitHub Pages)
-const outputPath = path.join(__dirname, '../index.html');
-fs.writeFileSync(outputPath, finalHtml);
-
-console.log('Success! Your index.html has been generated using Pug and MVC.');
+console.log('Success! Both index.html and about.html have been generated.');
